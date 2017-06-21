b765413e3cf7725a00e75b300adb2653eb67481b
shmck <shawn.j.mckay@gmail.com>
2017-06-21 11:53:37 -0700

    fix exports

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,14 @@
-import keykey from 'keykey';
-
-// Action types of the library
-export default keykey([
-  'API_SET_AXIOS_CONFIG',
-  'API_HYDRATE',
-  'API_WILL_CREATE',
-  'API_CREATED',
-  'API_CREATE_FAILED',
-  'API_WILL_READ',
-  'API_READ',
-  'API_READ_FAILED',
-  'API_WILL_UPDATE',
-  'API_UPDATED',
-  'API_UPDATE_FAILED',
-  'API_WILL_DELETE',
-  'API_DELETED',
-  'API_DELETE_FAILED'
-]);
+export const API_SET_AXIOS_CONFIG = 'API_SET_AXIOS_CONFIG';
+export const API_HYDRATE = 'API_HYDRATE';
+export const API_WILL_CREATE = 'API_WILL_CREATE';
+export const API_CREATED = 'API_CREATED';
+export const API_CREATE_FAILRED = 'API_CREATE_FAILED';
+export const API_WILL_READ = 'API_WILL_READ';
+export const API_READ = 'API_READ';
+export const API_READ_FAILED = 'API_READ_FAILED';
+export const API_WILL_UPDATE = 'API_WILL_UPDATE';
+export const API_UPDATED = 'API_UPDATED';
+export const API_UPDATE_FAILED = 'API_UPDATE_FAILED';
+export const API_WILL_DELETE = 'API_WILL_DELETE';
+export const API_DELETED = 'API_DELETED';
+export const API_DELETE_FAILED = 'API_DELETE_FAILED';
